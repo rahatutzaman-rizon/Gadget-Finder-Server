b2db349f2fed744b280325973cf9c93b191e0bb5
redwan-525 <redwantamim525@gmail.com>
2023-10-18 20:36:39 +0600

    post server to mongodb

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+ const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+ require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const  uri = "mongodb://product-redwan:6MFgJsNfzxmcGvtW@ac-7qfszrw-shard-00-00.hheptki.mongodb.net:27017,ac-7qfszrw-shard-00-01.hheptki.mongodb.net:27017,ac-7qfszrw-shard-00-02.hheptki.mongodb.net:27017/?ssl=true&replicaSet=atlas-13fbk7-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const productCollection = client.db('productDB').collection('product');
+
+    app.post('/product', async(req,res)=>{
+        const newProduct = req.body;
+       console.log(newProduct);
+       const result = await productCollection.insertOne(newProduct);
+       res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',async(req,res)=>{
+    res.send('server is running')
+})
+
+app.listen(port,()=>{
+    console.log(port);
+})
